4401dfa15a471fdffe15bbe43dfdd372e917c4ce
Yonas Fesehatsion <yone920@gmail.com>
2019-04-16 18:28:52 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+
+
+def reverse_each_word(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array.push(string.reverse)
+  end
+  return_array.join(" ")
+end
+

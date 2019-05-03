48df5d968adf3b7c52032f7f5535b003d23db80a
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-03 02:22:27 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,12 @@
 #.reverse is to reverse
 
 def reverse_each_word(sentence)
-  sentence = "Hello there and how are you?"
-  sentence.split.each do |word|
+    sentence.split.each do |word|
     word.reverse! 
-  end.join(sentence)
+  end.join(" ")
 end
 
-# def reverse_each_word(sentence)
-#   sentence.split.collect {|word| word.reverse}.join(" ")
-# end
+def reverse_each_word(sentence)
+   sentence.split.collect {|word| word.reverse}.join(" ")
+end
 

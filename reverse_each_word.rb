a182ecc7f757813a85819c3a727a67e5f3663fda
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-03 02:11:26 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+#.split is used to turn a string into an Array
+#.reverse is to reverse
+
+def reverse_each_word(sentence)
+  sentence = "Hello there and how are you?"
+  sentence.split.each do |word|
+    word.reverse! 
+  end.join |sentence|
+end
+
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|word| word.reverse}.join(" ")
+# end
+

34f2bb8f11d1260e616150021b62a8f9ffda0e74
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-03 02:12:36 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(sentence)
   sentence = "Hello there and how are you?"
   sentence.split.each do |word|
     word.reverse! 
-  end.join |sentence|
+  end.join(sentence)
 end
 
 # def reverse_each_word(sentence)
